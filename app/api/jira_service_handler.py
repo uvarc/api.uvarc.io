a28d5508828c64871678365c6eccfdc9b76781dc
Ravi Chamakuri <rkc7h@virginia.edu>
2019-07-31 12:37:29 -0400

    Pushing changes to enable flake8 autopep8 and formatted code to meet flake8 standards

--- app/api/jira_service_handler.py
+++ app/api/jira_service_handler.py
@@ -6,40 +6,46 @@ import requests
 class JiraServiceHandler:
     def __init__(self, app):
         self._auth = (app.config['JIRA_CONN_INFO']['CLIENT_ID'],
-            app.config['JIRA_CONN_INFO']['PASSWORD'])
+                      app.config['JIRA_CONN_INFO']['PASSWORD'])
         self._connect_host_url = 'https://{}:{}/rest/'.\
             format(app.config['JIRA_CONN_INFO']['HOST'],
                    app.config['JIRA_CONN_INFO']['PORT'])
         self._default_reporter = app.config['JIRA_CONN_INFO']['CLIENT_ID']
         self._project_info_lookup_dict = app.config['JIRA_PROJECT_INFO_LOOKUP']
-        self._project_request_type_lookup_dict = app.config['JIRA_PROJECT_REQUEST_TYPE_LOOKUP']
+        self._project_request_type_lookup_dict = app.config
+        ['JIRA_PROJECT_REQUEST_TYPE_LOOKUP']
 
     def createNewTicket(self,
                         reporter=None,
-                        participants = None,
-                        project_name = 'GENERAL_SUPPORT',
-                        request_type = 'GENERAL_SUPPORT_GET_IT_HELP',
+                        participants=None,
+                        project_name='GENERAL_SUPPORT',
+                        request_type='GENERAL_SUPPORT_GET_IT_HELP',
                         summary=None,
                         desc=None):
-        if(reporter == None):
+        if(reporter is None):
             reporter = self._default_reporter
-        if(participants == None):
+        if(participants is None):
             participants = [reporter]
         headers = {'content-type': 'application/json'}
         payload = json.dumps(
             {
-                "serviceDeskId": self._project_info_lookup_dict[project_name.upper()],
-                "requestTypeId": self._project_request_type_lookup_dict[request_type.upper()],
+                "serviceDeskId":
+                    self._project_info_lookup_dict[project_name.upper()],
+                "requestTypeId":
+                    self._project_request_type_lookup_dict[request_type.upper(
+                    )],
                 "requestFieldValues": {
                     "summary": summary,
                     "description": desc
-                },
+                        },
                 "requestParticipants": participants,
                 "raiseOnBehalfOf": reporter
             }
         )
-        r = requests.post(''.join([self._connect_host_url,'servicedeskapi/request']),
-                          headers=headers,
-                          data=payload,
-                          auth=self._auth)
+        r = requests.post(
+            ''.join([self._connect_host_url, 'servicedeskapi/request']),
+            headers=headers,
+            data=payload,
+            auth=self._auth
+        )
         return r.text
--- app/views.py
+++ app/views.py
@@ -1,14 +1,10 @@
 import json
-import copy
 
-from flask import abort, g, jsonify, make_response, redirect, request, url_for
+from flask import jsonify, make_response, request
 
-import boto3
-import flask_restful
-from app import app, auth, sso, limiter
-from botocore.exceptions import ClientError
-from flask_restful import reqparse
+from app import app, limiter
 from app.api.jira_service_handler import JiraServiceHandler
+
 
 def unauthorized():
     return make_response(jsonify(
@@ -16,20 +12,24 @@ def unauthorized():
          "message": "unauthorized"}
     ), 401)
 
-def _create_jira_support_request(form_elements_dict):
-  jira_service_handler = JiraServiceHandler(app)
-  project_ticket_route = app.config['JIRA_CATEGORY_PROJECT_ROUTE_DICT'][form_elements_dict['category'].strip()]
-  descStr = ''
-  for attrib in sorted(form_elements_dict):
-    descStr = ''.join([descStr,'{}={}\n'.format(str(attrib).upper(),form_elements_dict[attrib])])
 
-  return jira_service_handler.createNewTicket(
-    reporter = form_elements_dict['uid'],
-    project_name = project_ticket_route[0],
-    request_type = project_ticket_route[1],
-    summary = 'Customer request for {}'.format(form_elements_dict['category']),
-    desc = descStr
-  )
+def _create_jira_support_request(form_elements_dict):
+    jira_service_handler = JiraServiceHandler(app)
+    project_ticket_route = app.config['JIRA_CATEGORY_PROJECT_ROUTE_DICT']
+    [form_elements_dict['category'].strip()]
+    descStr = ''
+    for attrib in sorted(form_elements_dict):
+        descStr = ''.join([descStr, '{}={}\n'.format(
+            str(attrib).upper(), form_elements_dict[attrib])])
+
+    return jira_service_handler.createNewTicket(
+        reporter=form_elements_dict['uid'],
+        project_name=project_ticket_route[0],
+        request_type=project_ticket_route[1],
+        summary='Customer request for {}'.format(
+            form_elements_dict['category']),
+        desc=descStr
+    )
 
 
 @app.route('/rest/general-support-request/', methods=['POST'])
@@ -37,21 +37,27 @@ def _create_jira_support_request(form_elements_dict):
 @limiter.limit("2 per minute")
 def general_support_request():
     try:
-      response = json.loads(_create_jira_support_request(request.form))
+        response = json.loads(_create_jira_support_request(request.form))
 
-      return make_response(jsonify(
-          {'status': '200 OK',
-          'ticket_id': response['issueKey'],
-          'name' : request.form['name'],
-          'email': request.form['email'],
-          'uid': request.form['uid'],
-          'message': 'General support request successfully submitted'}
-      ), 200)
+        return make_response(jsonify(
+            {
+                'status': '200 OK',
+                'ticket_id': response['issueKey'],
+                'name': request.form['name'],
+                'email': request.form['email'],
+                'uid': request.form['uid'],
+                'message': 'General support request successfully submitted'
+            }
+        ), 200)
     except Exception as ex:
-      return make_response(jsonify(
-        {"status": "error",
-         "message": "Error submitting general support request : {}".format(str(ex))}
-      ), 501)
+        return make_response(jsonify(
+            {
+                "status": "error",
+                "message":
+                    "Error submitting general support request : {}".format(
+                        str(ex))
+            }
+        ), 501)
 
 
 @app.route('/rest/hpc-allocation-request/', methods=['POST'])
@@ -59,21 +65,26 @@ def general_support_request():
 @limiter.limit("1 per minute")
 def hpc_allocation_request():
     try:
-      request_form = dict(request.form.items())
-      request_form['category'] = "Rivanna HPC"
+        request_form = dict(request.form.items())
+        request_form['category'] = "Rivanna HPC"
 
-      response = json.loads(_create_jira_support_request(request_form))
+        response = json.loads(_create_jira_support_request(request_form))
 
-      return make_response(jsonify(
-          {'status': '200 OK',
-          'ticket_id': response['issueKey'],
-          'name' : request.form['name'],
-          'email': request.form['email'],
-          'uid': request.form['uid'],
-          'message': 'HPC allocation request successfully submitted'}
-      ), 200)
+        return make_response(jsonify(
+            {
+                'status': '200 OK',
+                'ticket_id': response['issueKey'],
+                'name': request.form['name'],
+                'email': request.form['email'],
+                'uid': request.form['uid'],
+                'message': 'HPC allocation request successfully submitted'
+            }
+        ), 200)
     except Exception as ex:
-      return make_response(jsonify(
-        {"status": "error",
-         "message": "Error submitting HPC allocation request : {}".format(str(ex))}
-      ), 501)
+        return make_response(jsonify(
+            {
+                "status": "error",
+                "message":
+                "Error submitting HPC allocation request : {}".format(str(ex))
+            }
+        ), 501)
