6190ca2a3c564125f44166162fe479d8958eef60
Ravi Chamakuri <rkc7h@virginia.edu>
2021-02-26 10:38:33 -0500

    Pushing change to update Allocation sponsort email

--- config/prod.py
+++ config/prod.py
@@ -86,7 +86,7 @@ MAIL_TIMEOUT = 10
 MAIL_SECRET_KEY = conn_info["SMTP"]["SECURE_KEY"]
 
 ALLOCATION_SPONSOR_EMAIL_LOOKUP = {
-    'cas': 'dh2t@virginia.edu',
+    'cas': 'tm7ds@virginia.edu',
     'seas': 'wbk3a@virginia.edu',
     'dsi': 'lpa2a@virginia.edu',
     'hs': 'jcm6t@virginia.edu',
